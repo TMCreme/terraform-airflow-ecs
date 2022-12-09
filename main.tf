eac21c7084186d37d40184d2263802d6d03b3ced
TMCreme <tonnybright123@yahoo.com>
2022-12-09 14:08:52 +0000

    working version for nginx

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+  region = var.default_region
 }
 
 # Create a VPC
 resource "aws_vpc" "airflow_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.eu_west_cidr_block
 }
 
 # Internet Gateway
@@ -22,7 +22,7 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.airflow_vpc.id
 
   tags = {
-    Name = "airflow-deploy-test"
+    Name = var.internet_gateway_name
   }
 }
 
@@ -37,40 +37,43 @@ resource "aws_default_route_table" "airflow_tb_test" {
   }
 
   tags = {
-    Name = "airflow_test_rt"
+    Name = var.route_table_rule
   }
 }
 
+# Create 2 subnets in different AZs, one each
+# first subnet
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.airflow_vpc.id
-  cidr_block        = "10.0.0.0/20"
-  availability_zone = "eu-west-1a"
+  cidr_block        = var.first_subnet_cidr
+  availability_zone = var.first_subnet_az
 
   tags = {
-    Name = "Test_Subnet1"
+    Name = var.first_subnet_name
   }
 }
 
+# Second Subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.airflow_vpc.id
-  cidr_block        = "10.0.16.0/20"
-  availability_zone = "eu-west-1b"
+  cidr_block        = var.second_subnet_cidr
+  availability_zone = var.second_subnet_az
 
   tags = {
-    Name = "Test_Subnet2"
+    Name = var.second_subnet_name
   }
 }
 
 # Create a Security Group 
 resource "aws_security_group" "alb_to_internet_comm_allow" {
-  name        = "alb_to_internet_comm_allow"
-  description = "Allow Airflow connections"
+  name        = var.security_group_to_internet_name
+  description = "Allow Internet traffic connections to Load balancer"
   vpc_id      = aws_vpc.airflow_vpc.id
 
   ingress {
     description      = "Allow Connection to http"
-    from_port        = 80
-    to_port          = 80
+    from_port        = var.internet_sg_from_port
+    to_port          = var.internet_sg_to_port
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
@@ -85,21 +88,21 @@ resource "aws_security_group" "alb_to_internet_comm_allow" {
   }
 
   tags = {
-    Name = "internet_traffic_to"
+    Name = var.security_group_to_internet_name
   }
 }
 
 # Create another Security Group to allow traffic from 
 # the internet facing sg to ecs 
 resource "aws_security_group" "alb_to_ecs_traffic" {
-  name        = "alb_to_ecs_traffic"
+  name        = var.security_group_to_ecs_name
   description = "Allow SG connections"
   vpc_id      = aws_vpc.airflow_vpc.id
 
   ingress {
     description     = "Allow Connection from SG to ECS"
-    from_port       = 80
-    to_port         = 80
+    from_port       = var.ecs_to_lb_sg_from_port
+    to_port         = var.ecs_to_lb_sg_to_port
     protocol        = "tcp"
     security_groups = [aws_security_group.alb_to_internet_comm_allow.id]
     # ipv6_cidr_blocks = ["::/0"]
@@ -114,13 +117,13 @@ resource "aws_security_group" "alb_to_ecs_traffic" {
   }
 
   tags = {
-    Name = "alb_to_traffic"
+    Name = var.security_group_to_ecs_name
   }
 }
 
 # Create an application load balancer 
 resource "aws_lb" "alb_ecs_airflow" {
-  name               = "alb-ecs-airflow"
+  name               = var.alb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_to_internet_comm_allow.id]
@@ -135,21 +138,23 @@ resource "aws_lb" "alb_ecs_airflow" {
   # }
 
   tags = {
-    Environment = "airflowtest"
+    Environment = "test"
   }
 }
 
+# Create a target group for the load balancer
 resource "aws_lb_target_group" "alb_ecs_airflow_tg" {
-  name        = "alb-ecs-airflow-tg"
+  name        = var.target_group_name
   target_type = "ip"
-  port        = 80
+  port        = var.target_group_port
   protocol    = "HTTP"
   vpc_id      = aws_vpc.airflow_vpc.id
 }
 
+# Create a listener for the Load balancer
 resource "aws_lb_listener" "alb_ecs_airflow" {
   load_balancer_arn = aws_lb.alb_ecs_airflow.arn
-  port              = "80"
+  port              = var.listener_port
   protocol          = "HTTP"
 
   default_action {
@@ -169,34 +174,59 @@ resource "aws_lb_listener" "alb_ecs_airflow" {
 
 }
 
+# CloudWatch 
+resource "aws_cloudwatch_log_group" "log" {
+  name              = var.cloudwatch_ecs_log_name
+  retention_in_days = var.cloudwatch_log_retention_days
+}
 
+# ECS Cluster creation
 resource "aws_ecs_cluster" "test_airflow" {
-  name = "test_airflow"
+  name = var.ecs_cluster_name
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      logging = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.log.name
+      }
+    }
+  }
 }
 
 resource "aws_ecs_cluster_capacity_providers" "test_airflowcp" {
   cluster_name = aws_ecs_cluster.test_airflow.name
 
   capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
 }
 
-resource "aws_cloudwatch_log_group" "log" {
-  name              = "ecs/nginx"
-  retention_in_days = 14
-}
 
-resource "aws_ecs_task_definition" "nginxtestfamily" {
-  family                   = "nginxtestfamily"
+resource "aws_ecs_task_definition" "airflowtestfamily" {
+  family                   = var.ecs_task_definition_family_name
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = var.ecs_container_cpu
+  memory                   = var.ecs_container_memory
+  execution_role_arn       = var.ecs_task_execution_role_arn
   container_definitions = jsonencode([
     {
-      name      = "nginxtest"
-      image     = "nginx:latest"
-      cpu       = 1024
-      memory    = 2048
+      name      = var.ecs_task_container_name
+      image     = var.ecs_container_image
+      cpu       = var.ecs_container_cpu
+      memory    = var.ecs_container_memory
       essential = true
       portMappings = [
         {
@@ -210,62 +240,24 @@ resource "aws_ecs_task_definition" "nginxtestfamily" {
 }
 
 
-resource "aws_ecs_service" "nginxtestservice" {
-  name            = "nginxtestservice"
+resource "aws_ecs_service" "airflowtestservice" {
+  name            = var.ecs_service_name
   cluster         = aws_ecs_cluster.test_airflow.id
-  task_definition = "${aws_ecs_task_definition.nginxtestfamily.family}:${aws_ecs_task_definition.nginxtestfamily.revision}"
-  desired_count   = 2
-  #   iam_role        = "arn:aws:iam::291509689978:role/ecsTaskExecutionRole"
-  #   depends_on      = [aws_iam_role_policy.foo]
-#   "${aws_ecs_task_definition.my_task.family}:${aws_ecs_task_definition.my_task.revision}"
-  launch_type = "FARGATE"
+  task_definition = "${aws_ecs_task_definition.airflowtestfamily.family}:${aws_ecs_task_definition.airflowtestfamily.revision}"
+  desired_count   = 1
+  launch_type = var.ecs_service_launch_type
 
   load_balancer {
     target_group_arn = aws_lb_target_group.alb_ecs_airflow_tg.arn
-    container_name   = "nginxtest"
+    container_name   = var.ecs_task_container_name
     container_port   = 80
   }
 
   network_configuration {
     subnets         = [aws_subnet.public.id, aws_subnet.public_subnet.id]
     security_groups = [aws_security_group.alb_to_ecs_traffic.id]
+    assign_public_ip = true
   }
 }
-
-# resource "aws_ecs_task_set" "nginxtesttask" {
-#   service         = aws_ecs_service.nginxtestservice.id
-#   cluster         = aws_ecs_cluster.test_airflow.id
-#   task_definition = aws_ecs_task_definition.nginxtestfamily.arn
-
-#   load_balancer {
-#     target_group_arn = aws_lb_target_group.alb_ecs_airflow_tg.arn
-#     container_name   = "nginx"
-#     container_port   = 80
-#   }
-# }
-
-
-
-# module "task_definition" {
-#   source = "../.."
-
-#   execution_role   = null
-#   image            = "nginx"
-#   image_tag        = var.image_tag
-#   memory           = 64
-#   log_group        = aws_cloudwatch_log_group.log.name
-#   ports            = var.ports
-#   network_mode     = var.network_mode
-#   task_role        = "ecs-task-role"
-#   namespace        = var.namespace
-#   name             = var.name
-#   type             = "default"
-#   health_check     = [["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost/ || exit 1"], 30, 2, 3]
-#   volumes          = var.volumes
-#   mounts           = var.mounts
-#   task_environment = var.task_environment
-#   docker_labels    = var.docker_labels
-# }
-
 
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,208 @@
+variable "default_region" {
+  description = "The default region for resources"
+  type = string
+  default = "eu-west-1"
+}
+
+variable "eu_west_cidr_block" {
+    description = "Cidr block for VPC"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "internet_gateway_name" {
+    description = "Name of my internet gateway"
+    type = string
+    default = "airflow-deploy-test"
+}
+
+variable "route_table_rule" {
+    description = "Name of the route table route"
+    type = string 
+    default = "airflow_test_rt"
+}
+
+# First subnet 
+variable "first_subnet_cidr" {
+    description = "Cidr block for Subnet a "
+    type = string
+    default = "10.0.0.0/20"
+}
+
+variable "first_subnet_az" {
+    description = "Availability zone for the first subnet"
+    type = string
+    default = "eu-west-1a"
+}
+
+variable "first_subnet_name" {
+    description = "Name of the first subnet"
+    type = string 
+    default = "Test_Subnet1"
+}
+
+# Second Subnet 
+variable "second_subnet_cidr" {
+    description = "Cidr block for Subnet b"
+    type = string
+    default = "10.0.16.0/20"
+}
+
+variable "second_subnet_az" {
+    description = "Availability zone for the Second subnet"
+    type = string
+    default = "eu-west-1b"
+}
+
+variable "second_subnet_name" {
+    description = "Name of the Second subnet"
+    type = string 
+    default = "Test_Subnet2"
+}
+
+# Security Groups
+# First security group allows traffic to the ALB from internet
+variable "security_group_to_internet_name" {
+    description = "Security group that allow internet traffic to ALB"
+    type = string
+    default = "alb_to_internet_comm_allow"  
+}
+
+variable "internet_sg_from_port" {
+    description = "Starting value for port range on the SG for internet traffic"
+    type =  number
+    default = 80
+}
+
+variable "internet_sg_to_port" {
+    description = "End value for port range on the SG for internet traffic"
+    type =  number
+    default = 80
+}
+
+# Second security group allows traffic from the first SG to the ECS target
+variable "security_group_to_ecs_name" {
+    description = "Security group that allow ALB traffic to ECS target"
+    type = string
+    default = "ecs_to_alb_traffic_allow"  
+}
+
+variable "ecs_to_lb_sg_from_port" {
+    description = "Starting value for port range on the SG for ALB traffic"
+    type =  number
+    default = 80
+}
+
+variable "ecs_to_lb_sg_to_port" {
+    description = "End value for port range on the SG for ALB traffic"
+    type =  number
+    default = 80
+}
+
+# Load balancer
+variable "alb_name" {
+    description = "Name of the load balancer"
+    type = string
+    default = "alb-ecs-airflow"
+}
+
+# Target Group
+variable "target_group_name" {
+    description = "name of the target group"
+    type = string
+    default = "alb-ecs-airflow-tg"
+  
+}
+
+variable "target_group_port" {
+    description = "Port of the target group"
+    type = number
+    default = 80 
+}
+
+# Listener
+variable "listener_port" {
+    description = "Port of the listener"
+    type = number
+    default = 80
+}
+
+# CloudWatch
+variable "cloudwatch_ecs_log_name" {
+    description = "Name for the Log Group on Cloudwatch"
+    type = string
+    default = "ecs/airflow"
+}
+
+variable "cloudwatch_log_retention_days" {
+    description = "Number of days to retain the logs"
+    type = number
+    default = 3
+}
+
+# ECS Cluster
+variable "ecs_cluster_name" {
+    description = "name of the Cluster"
+    type = string
+    default = "test_airflow"
+}
+
+
+variable "ecs_task_definition_family_name" {
+    description = "name of the task family"
+    type = string
+    default = "airflowtestfamily"
+}
+
+variable "ecs_task_execution_role_arn" {
+    description = "IAM Execution role for the ECS task"
+    type = string
+    default = "arn:aws:iam::291509689978:role/ecsTaskExecutionRole"    
+}
+
+variable "ecs_task_container_name" {
+    description = "Name of the container under the task"
+    type = string
+    default = "airflowtest"
+}
+
+variable "ecs_container_image" {
+    description = "The image to pull for deployment"
+    type = string
+    default = "nginx"
+}
+
+variable "ecs_container_cpu" {
+    description = "CPU Size for the container"
+    type = number
+    default = 1024
+}
+
+variable "ecs_container_memory" {
+    description = "Memory Size allocation for the Container"
+    type = number 
+    default = 2048
+}
+
+# ECS Service
+variable "ecs_service_name" {
+    description = "Name of the service under the cluster"
+    type = string
+    default = "airflowtestservice"
+}
+
+variable "ecs_service_launch_type" {
+    description = "Launch type for the ECS Service"
+    type = string
+    default = "FARGATE"
+  
+}
+
+
+
+
+
+
 
 
 
